4acb8ee956aabdfd315d48ebc5b81a827bd8ecda
MahshadOliaie <mahshadoliaie@gmail.com>
2024-03-06 00:40:40 +0330

    tailwind added

--- src/components/PersonalInfo/PersonalInfo.jsx
+++ src/components/PersonalInfo/PersonalInfo.jsx
@@ -1,11 +1,10 @@
 
-
 function PersonalInfo() {
     return (
         <>
             <h1>Personal Info</h1>
             <form>
-                <input type="text" placeholder="name" />
+                <input type="text" placeholder="name"/>
                 <input type="email" placeholder="example@email.com" />
                 <input type="password" placeholder="password" />
             </form>
