4440c9159a18e6811f982891ce2ff4aaa453757a
MahshadOliaie <mahshadoliaie@gmail.com>
2024-03-06 01:02:01 +0330

    usinf tailwinf

--- src/components/PersonalInfo/PersonalInfo.jsx
+++ src/components/PersonalInfo/PersonalInfo.jsx
@@ -1,14 +1,14 @@
 
 function PersonalInfo() {
     return (
-        <>
-            <h1>Personal Info</h1>
-            <form>
-                <input type="text" placeholder="name"/>
-                <input type="email" placeholder="example@email.com" />
-                <input type="password" placeholder="password" />
+        <div className="flex flex-col items-center">
+            <h1 className="text-2xl font-medium my-4">Personal Info</h1>
+            <form className="flex flex-col w-4/5 gap-4">
+                <input type="text" placeholder="name" className="bg-gray-100 p-3"/>
+                <input type="email" placeholder="example@email.com" className="bg-gray-100 p-3" />
+                <input type="password" placeholder="password" className="bg-gray-100 p-3" />
             </form>
-        </>
+        </div>
     )
 }
 
