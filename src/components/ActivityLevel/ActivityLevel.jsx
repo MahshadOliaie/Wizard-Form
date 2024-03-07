1c6998783222fd6da0f9aae2f1c33b4b4c163437
MahshadOliaie <mahshadoliaie@gmail.com>
2024-03-07 11:38:44 +0330

    debug

--- src/components/ActivityLevel/ActivityLevel.jsx
+++ src/components/ActivityLevel/ActivityLevel.jsx
@@ -8,16 +8,25 @@ import { useNavigate } from "react-router-dom"
 
 function ActivityLevel({ setStep }) {
     const nav = useNavigate()
-    const {register, handleSubmit, setValue, formState: { errors } } = useForm()
+    const { setError, handleSubmit, getValues, clearErrors, setValue, formState: { errors } } = useForm({
+        shouldUnregister: false,
+    })
     const { setActivityLevel } = useContext(Information)
 
     async function submitActivity(data) {
-        setActivityLevel(data)
-        await nav('/4')
+        if (getValues("activityLevel")) {
+            setActivityLevel(data)
+            await nav('/4')
+        } else {
+            setError("activityLevel", {
+                message: "select one item"
+            })
+        }
     }
 
     function handleChange() {
         setValue("activityLevel", event.target.value)
+        clearErrors("activityLevel")
     }
 
     return (
@@ -25,37 +34,27 @@ function ActivityLevel({ setStep }) {
             <h1 className="text-2xl font-medium my-10">select your activity level</h1>
             <form className="flex flex-col gap-7">
                 <div className="gap-2 flex" >
-                    <input onChange={handleChange} {...register("activityLevel" , {
-                        required : "select one activity level"
-                    })} type="radio" name="rate" id="one" value={"one"} />
+                    <input onChange={handleChange} type="radio" name="rate" id="one" value={"one"} />
                     <label htmlFor="one">Sedentary <span className="text-gray-500">(Little or no Exercise/ desk job)</span></label>
                 </div>
 
                 <div className="gap-2 flex">
-                    <input onChange={handleChange} {...register("activityLevel" , {
-                        required : "select one activity level"
-                    })} type="radio" name="rate" id="two" value={"two"} />
+                    <input onChange={handleChange} type="radio" name="rate" id="two" value={"two"} />
                     <label htmlFor="two">Lightly active <span className="text-gray-500">(1-3 days Exercise per week)</span></label>
                 </div>
 
                 <div className="gap-2 flex">
-                    <input onChange={handleChange} {...register("activityLevel" , {
-                        required : "select one activity level"
-                    })} type="radio" name="rate" id="three" value={"three"} />
+                    <input onChange={handleChange} type="radio" name="rate" id="three" value={"three"} />
                     <label htmlFor="three">Moderately active <span className="text-gray-500">(3-5 days Exercise/ desk job)</span></label>
                 </div>
 
                 <div className="gap-2 flex">
-                    <input onChange={handleChange} {...register("activityLevel" , {
-                        required : "select one activity level"
-                    })} type="radio" name="rate" id="four" value={"four"} />
+                    <input onChange={handleChange} type="radio" name="rate" id="four" value={"four"} />
                     <label htmlFor="four">Very active <span className="text-gray-500">(4-5 days Exercise/ Physical job)</span></label>
                 </div>
 
                 <div className="gap-2 flex">
-                    <input onChange={handleChange} {...register("activityLevel" , {
-                        required : "select one activity level"
-                    })} type="radio" name="rate" id="five" value={"five"} />
+                    <input onChange={handleChange} type="radio" name="rate" id="five" value={"five"} />
                     <label htmlFor="five">Extremely active <span className="text-gray-500">(7 days Exercise/ Physical job)</span></label>
                 </div>
 
