eb279daa96822a9fc362308526c1eef851ac9314
MahshadOliaie <mahshadoliaie@gmail.com>
2024-03-07 09:44:58 +0330

    fix formla

--- src/components/ShowCalory/ShowCalory.jsx
+++ src/components/ShowCalory/ShowCalory.jsx
@@ -3,10 +3,6 @@ import Information from "../../context/Information/Information"
 
 
 function ShowCalory() {
-    const { physicalInfo, goal, activityLevel } = useContext(Information)
-    let formula = (10 * physicalInfo.weight) + (6.25 * physicalInfo.height) - (5 * physicalInfo.age)
-
-
     const activityLevelsNumber = {
         "one": 1.2,
         "two": 1.375,
@@ -15,12 +11,23 @@ function ShowCalory() {
         "five": 1.9
     }
 
+    const { physicalInfo, goal, activityLevel } = useContext(Information)
+    let formula = (10 * physicalInfo.weight) + (6.25 * physicalInfo.height) - (5 * physicalInfo.age)
+    let BMR = (physicalInfo.gender == "male") ? formula + 5 : formula - 161
+    let maintainCalory = Math.ceil(BMR * activityLevelsNumber[activityLevel.activityLevel])
+
+
+    const goals = {
+        "gain": maintainCalory + (maintainCalory / 10),
+        "cut": maintainCalory - (maintainCalory / 10),
+        "maintain": maintainCalory
+    }
 
     return (
         <>
             <p className="text-2xl font-medium my-5">
                 you should take
-                <span className="text-3xl">{Math.ceil(((physicalInfo.gender == "male") ? formula + 5 : formula - 161) * activityLevelsNumber[activityLevel.activityLevel])}</span>
+                <span className="text-3xl">{Math.ceil(goals[goal.goal])}</span>
                 calories per day</p>
         </>
     )
