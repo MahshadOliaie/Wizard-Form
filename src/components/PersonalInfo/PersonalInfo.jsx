f4977dea1cdb0a7838cd99c82a1f9a3deaa43637
MahshadOliaie <mahshadoliaie@gmail.com>
2024-03-07 11:41:46 +0330

    style changes

--- src/components/PersonalInfo/PersonalInfo.jsx
+++ src/components/PersonalInfo/PersonalInfo.jsx
@@ -22,30 +22,37 @@ function PersonalInfo() {
             <h1 className="text-2xl font-medium my-5">Personal Info</h1>
             <form className="flex flex-col gap-7">
 
-                <input {...register("name", {
-                    required: "fill this input",
-                })} type="text" placeholder="name" className="bg-gray-50 p-3 shadow-inner rounded-xl" />
-                {errors.name && <p className="text-red-300">{errors.name.message}</p>}
+                <div>
+                    <input {...register("name", {
+                        required: "fill this input",
+                    })} type="text" placeholder="name" className="bg-gray-50 w-full p-3 shadow-inner rounded-xl" />
+                    {errors.name && <p className="text-red-300">{errors.name.message}</p>}
+                </div>
 
-                <input {...register("email", {
-                    required: "email is required",
-                    validate: (value) => {
-                        if (!value.match(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)) {
-                            return "invalid email!"
+
+                <div>
+                    <input {...register("email", {
+                        required: "email is required",
+                        validate: (value) => {
+                            if (!value.match(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)) {
+                                return "invalid email!"
+                            }
+                            return true
                         }
-                        return true
-                    }
-                })} type="email" placeholder="example@email.com" className="bg-gray-50 p-3 shadow-inner rounded-xl" />
-                {errors.email && <p className="text-red-300">{errors.email.message}</p>}
+                    })} type="email" placeholder="example@email.com" className="bg-gray-50 w-full p-3 shadow-inner rounded-xl" />
+                    {errors.email && <p className="text-red-300">{errors.email.message}</p>}
+                </div>
 
-                <input {...register("password", {
-                    required: "password is required",
-                    minLength: {
-                        value: 8,
-                        message: "Password must have at least 8 characters"
-                    }
-                })} type="password" placeholder="password (at least 8 characters)" className="bg-gray-50 p-3 shadow-inner rounded-xl" />
-                {errors.password && <p className="text-red-300">{errors.password.message}</p>}
+                <div>
+                    <input {...register("password", {
+                        required: "password is required",
+                        minLength: {
+                            value: 8,
+                            message: "Password must have at least 8 characters"
+                        }
+                    })} type="password" placeholder="password (at least 8 characters)" className="bg-gray-50 w-full p-3 shadow-inner rounded-xl" />
+                    {errors.password && <p className="text-red-300">{errors.password.message}</p>}
+                </div>
             </form>
             <NextBtn onClick={handleSubmit(submit)} />
         </>
