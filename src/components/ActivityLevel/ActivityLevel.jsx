584c4d797105be1c9eda289f27c412ac7b77ce17
MahshadOliaie <mahshadoliaie@gmail.com>
2024-03-07 09:30:27 +0330

    fixed activity level form

--- src/components/ActivityLevel/ActivityLevel.jsx
+++ src/components/ActivityLevel/ActivityLevel.jsx
@@ -6,12 +6,16 @@ import PreviousBtn from "../PreviousBtn/PreviousBtn"
 
 
 function ActivityLevel({setStep}) {
-    const { register, handleSubmit } = useForm()
+    const { register, handleSubmit , setValue } = useForm()
     const { setActivityLevel } = useContext(Information)
 
-    function submit(data) {
+    function submitActivity(data) {
         console.log(data)
         setActivityLevel(data)
+    }
+
+    function handleChange(){
+        setValue("activityLevel" , event.target.value)
     }
 
     return (
@@ -19,34 +23,34 @@ function ActivityLevel({setStep}) {
             <h1 className="text-2xl font-medium my-10">select your activity level</h1>
             <form className="flex flex-col gap-7">
                 <div className="gap-2 flex" >
-                    <input {...register("activityLevel")} type="radio" name="rate" id="one" value={"one"} />
+                    <input onChange={handleChange} type="radio" name="rate" id="one" value={"one"} />
                     <label htmlFor="one">Sedentary <span className="text-gray-500">(Little or no Exercise/ desk job)</span></label>
                 </div>
 
                 <div className="gap-2 flex">
-                    <input {...register("activityLevel")} type="radio" name="rate" id="two" value={"two"} />
+                    <input onChange={handleChange} type="radio" name="rate" id="two" value={"two"} />
                     <label htmlFor="two">Lightly active <span className="text-gray-500">(1-3 days Exercise per week)</span></label>
                 </div>
 
                 <div className="gap-2 flex">
-                    <input {...register("activityLevel")} type="radio" name="rate" id="three" value={"three"} />
+                    <input onChange={handleChange} type="radio" name="rate" id="three" value={"three"} />
                     <label htmlFor="three">Moderately active <span className="text-gray-500">(3-5 days Exercise/ desk job)</span></label>
                 </div>
 
                 <div className="gap-2 flex">
-                    <input {...register("activityLevel")} type="radio" name="rate" id="four" value={"four"} />
+                    <input onChange={handleChange} type="radio" name="rate" id="four" value={"four"} />
                     <label htmlFor="four">Very active <span className="text-gray-500">(4-5 days Exercise/ Physical job)</span></label>
                 </div>
 
                 <div className="gap-2 flex">
-                    <input {...register("activityLevel")} type="radio" name="rate" id="five" value={"five"} />
+                    <input onChange={handleChange} type="radio" name="rate" id="five" value={"five"} />
                     <label htmlFor="five">Extremely active <span className="text-gray-500">(7 days Exercise/ Physical job)</span></label>
                 </div>
             </form>
 
             <div className="flex justify-between">
                 <PreviousBtn step={2}  setStep={setStep} />
-                <NextBtn onClick={handleSubmit(submit)} step={4} />
+                <NextBtn onClick={handleSubmit(submitActivity)} step={4} />
             </div>
         </>
     )
--- src/components/ShowCalory/ShowCalory.jsx
+++ src/components/ShowCalory/ShowCalory.jsx
@@ -2,13 +2,26 @@ import { useContext } from "react"
 import Information from "../../context/Information/Information"
 
 
-function ShowCalory(){
-    const {physicalInfo , goal , activityLevel} = useContext(Information)
-    let formula = (10* physicalInfo.weight)+ (6.25*physicalInfo.height)-(5*physicalInfo.age)
-    console.log(activityLevel.activityLevel)
+function ShowCalory() {
+    const { physicalInfo, goal, activityLevel } = useContext(Information)
+    let formula = (10 * physicalInfo.weight) + (6.25 * physicalInfo.height) - (5 * physicalInfo.age)
+
+
+    const activityLevelsNumber = {
+        "one": 1.2,
+        "two": 1.375,
+        "three": 1.55,
+        "four": 1.725,
+        "five": 1.9
+    }
+
+
     return (
         <>
-        <p className="text-2xl font-medium my-5">you should take <span className="text-3xl">{(physicalInfo.gender == "male")? formula+5 : formula-161}</span> calories per day</p>
+            <p className="text-2xl font-medium my-5">
+                you should take
+                <span className="text-3xl">{Math.ceil(((physicalInfo.gender == "male") ? formula + 5 : formula - 161) * activityLevelsNumber[activityLevel.activityLevel])}</span>
+                calories per day</p>
         </>
     )
 }
